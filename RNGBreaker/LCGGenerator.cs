7db6682bf5c08b9c0bebac138aac0034489ea909
Yelyzaveta Hrybniak <y.s.hrybniak@globallogic.com>
2021-12-07 17:35:56 +0200

    break LCG Generator

---
+++ RNGBreaker/LCGGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNGBreaker
+{
+
+    public static class LCGGenerator
+    {
+        private static int a = 1234561;
+        private static int c = 654321;
+        public static long m = (long) Math.Pow(2, 32);
+        private static long seed = 13579;
+
+        public static long Next()
+        {
+            seed = (a * seed + c) % m;
+            return seed;
+        }
+    }
+}
---
+++ RNGBreaker/LongExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNGBreaker
+{
+    public static class LongExtensions
+    {
+        public static long ModInversion(this long a, long p)
+        {
+            var result = EGCD(a, p);
+            if (result.Item1 == 1)
+            {
+                return result.Item2 % p;
+            }
+            else
+            {
+                throw new InvalidOperationException("Numbers should be coprime");
+            }
+        }
+
+        private static Tuple<long, long, long> EGCD(long a, long p)
+        {
+            if (a == 0)
+            {
+                return new (p, 0, 1);
+            }
+            else
+            {
+                var result = EGCD(p % a, a);
+                return new(
+                    result.Item1,
+                    result.Item3 - (p / a) * result.Item2,
+                    result.Item2);
+            }
+        }
+    }
+}
