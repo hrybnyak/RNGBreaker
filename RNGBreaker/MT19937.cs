66e5d1c7748c84512256d0dc2fbf0a0e3d9269f8
Yelyzaveta Hrybniak <y.s.hrybniak@globallogic.com>
2021-12-08 13:26:33 +0200

    add mt19937

---
+++ RNGBreaker/MT19937.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNGBreaker
+{
+    public class MT19937
+    {
+        public const int w = 32;
+        public const int n = 624;
+        public const int f = 1812433253;
+        public const int m = 397;
+        public const int r = 31;
+        public const uint a = 0x9908B0DF;
+        public const uint d = 0xFFFFFFFF;
+        public const uint c = 0xEFC60000;
+        public const uint b = 0x9D2C5680;
+        public const int u = 11;
+        public const int s = 7;
+        public const int t = 15;
+        public const int l = 18;
+        private int[] X = new int[n];
+        private int counter = 0;
+
+        public MT19937(int seed)
+        {
+            X[0] = seed;
+            for (int i = 1; i < n; i++)
+            {
+                X[i] = ((X[i - 1] ^ (X[i - 1] >> (w - 2))) + i);
+            }
+            Twist();
+        }
+
+        private void Twist()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                unchecked
+                {
+                    var lowerMask = (1 << r) - 1;
+                    var upperMask = (~lowerMask) & ((1 << w) - 1);
+                    int temp = (X[i] & upperMask) + X[(i + 1) % n] & lowerMask;
+                    int tmpA = temp >> 1;
+                    if (temp % 2 != 0)
+                    {
+                        tmpA = tmpA ^ (int)a;
+                    }
+                    X[i] = (X[(i + m) % n] ^ tmpA);
+                }
+            }
+            counter = 0;
+        }
+
+        public int Next()
+        {
+            if (counter == n)
+            {
+                Twist();
+            }
+            var y = X[counter];
+            y = (int)(y ^ ((y >> u) & d));
+            y = (int)(y ^ ((y << s) & b));
+            y = (int)(y ^ ((y << t) & c));
+            y = y ^ (y >> l);
+            counter++;
+            return y;
+        }
+
+    }
+}
--- RNGBreaker/Program.cs
+++ RNGBreaker/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RNGBreaker
@@ -8,9 +8,28 @@ namespace RNGBreaker
     {
         static async Task Main(string[] args)
         {
-            var casinoClient = new CasinoHttpClient();
-            var rngBreaker = new RNGBreaker(casinoClient);
-            await rngBreaker.BreakLcg();
+            //var casinoClient = new CasinoHttpClient();
+            //var rngBreaker = new RNGBreaker(casinoClient);
+            //await rngBreaker.BreakLcg();
+            var start = 50;
+            var end = 150;
+            Random rand = new Random();
+            Thread.Sleep(rand.Next(start, end));
+            var time = (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            var mt = new MT19937(time);
+            Thread.Sleep(rand.Next(start, end));
+            var value = mt.Next();
+            var timeNow = (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            for (int i = timeNow - start; i < timeNow + end; i++)
+            {
+                var generator = new MT19937(i);
+                var number = generator.Next();
+                if (number == value)
+                {
+                    Console.WriteLine("Seed: " + number);
+                }
+            }
+
         }
     }
 }
